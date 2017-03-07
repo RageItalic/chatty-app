f14a81bd44c9ccf2e24ae199efc30b0e053b5d09
parth <parth@parths-MacBook-Air.local>
2017-03-07 17:20:37 -0500

    entering a custom message is now possible

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ class App extends Component {
       currentUser: {name: 'Bob'},
       messages:[
         {
+          id:1,
           username: "Bob",
           content: "Has anyone seen my marbles?"
         },
         {
+          id: 2,
           username: "Anonymous",
           content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
         }
@@ -24,14 +26,22 @@ class App extends Component {
     setTimeout(() => {
       console.log("Simulating incoming message");
       // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
+
     }, 3000)
   }
 
+  pressEnter = (message) => {
+       const newMessage = {id: this.state.messages.length + 1, username: this.state.currentUser.name, content: message};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages})
+      //this.setState(newMessage)
+  }
+
+  ayerhisuf = () => {
+
+  }
 
   render() {
     return (
@@ -40,7 +50,7 @@ class App extends Component {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messages={this.state.messages} />
-        <ChatBar currentUser={this.state.currentUser}/>
+        <ChatBar currentUser={this.state.currentUser} pressEnter={this.pressEnter}/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+  constructor() {
+    super()
+    this.state = { message: ''}
+  }
+
+  handleMessageChange = (event) => {
+    this.setState({message: event.target.value})
+  }
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      this.props.pressEnter(this.state.message)
+      this.setState({message: ''})
+    }
+  }
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" defaultValue={this.props.currentUser.name}/>
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" defaultValue={this.props.currentUser.name} />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" value={this.state.message} onChange={this.handleMessageChange} onKeyPress={this.handleKeyPress} />
       </footer>
     );
   }
